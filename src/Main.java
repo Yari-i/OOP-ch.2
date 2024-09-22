b77b16ce6653e7b3bcd80059b22660d6f18754b6
Yari-i <tipo47181@gmail.com>
2024-09-23 01:54:12 +0500

    HW DONE

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import Service.ServiceStation;
+import Transport.Bicycle;
+import Transport.Car;
+import Transport.Truck;
+
 public class Main {
     public static void main(String[] args) {
+        ServiceStation serviceStation = new ServiceStation();
+
+        Truck truck = new Truck("ТАТРА", 6);
+        Car car = new Car("LADA", 4);
+        Bicycle bicycle = new Bicycle("BMX", 2);
+
+        serviceStation.service(truck);
+        serviceStation.service(car);
+        serviceStation.service(bicycle);
 
     }
 }
---
+++ src/Transport/Transport.java
@@ -0,0 +1,33 @@
+package Transport;
+
+import Service.Serviceable;
+
+public abstract class Transport implements Serviceable {
+
+    private final String modelName;
+    private final int wheelsCount;
+
+    public Transport(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+    private void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    @Override
+    public void service() {
+        System.out.println("Обслуживаем " + modelName);
+        for (int i = 0; i < wheelsCount; i++) {
+            updateTyre();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Transport{" +
+                "modelName='" + modelName + '\'' +
+                ", wheelsCount=" + wheelsCount +
+                '}';
+    }
+}
